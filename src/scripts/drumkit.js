dd26dd958d6d04fd2742b1d4241fe1b00db8c34e
gonsaje <song.jaehyun@gmail.com>
2020-05-14 16:31:37 -0400

    script cleanup

--- src/scripts/drumkit.js
+++ src/scripts/drumkit.js
@@ -1,6 +1,7 @@
 import Tone from 'tone';
 
 let kick1 = new Audio("../sounds/808/kick1.wav");
+let kick2 = new Audio("../sounds/808/kick2.wav");
 let rimshot = new Audio("../sounds/808/rimshot.wav");
 let clap = new Audio("../sounds/808/handclap.wav");
 let snare = new Audio("../sounds/808/snare.wav");
@@ -14,60 +15,21 @@ let hi_conga = new Audio("../sounds/808/hi_conga.wav");
 let claves = new Audio("../sounds/808/claves.wav");
 let cowbell = new Audio("../sounds/808/cowbell.wav");
 let maracas = new Audio("../sounds/808/maracas.wav");
-let kick2 = new Audio("../sounds/808/kick2.wav");
 
-// let audio = new Tone.node
-// let 
-// let drums = new Tone.Sampler({
-// });
-//   // })
-// class Drums{
-
-//   // 
-//   // constructor(props){
-    
-//   //   let kick1 = new Audio("../sounds/808/kick1.wav");
-//   //   let rimshot = new Audio("../sounds/808/rimshot.wav");
-//   //   let clap = new Audio("../sounds/808/handclap.wav");
-//   //   let snare = new Audio("../sounds/808/snare.wav");
-//   //   let cl_hihat = new Audio("../sounds/808/cl_hihat.wav");
-//   //   let open_hihat = new Audio("../sounds/808/open_hh.wav");
-//   //   let crash = new Audio("../sounds/808/crashcym.wav");
-//   //   let tom = new Audio("../sounds/808/tom1.wav");
-//   //   let hightom = new Audio("../sounds/808/hightom.wav");
-//   //   let conga = new Audio("../sounds/808/conga1.wav");
-//   //   let hi_conga = new Audio("../sounds/808/hi_conga.wav");
-//   //   let claves = new Audio("../sounds/808/claves.wav");
-//   //   let cowbell = new Audio("../sounds/808/cowbell.wav");
-//   //   let maracas = new Audio("../sounds/808/maracas.wav");
-//   //   let kick2 = new Audio("../sounds/808/kick2.wav");
-     
-//   // }
-  
-//   // play(sound){
-
-//   // }
-
-//   // render(){
-//   //   return (
-//   //     <div>
-//   //       <button></button>
-//   //       <button>Rimshot</button>
-//   //       <button>Clap</button>
-//   //       <button>Snare</button>
-//   //       <button>Closed Hi-Hat</button>
-//   //       <button>Open Hi-Hat</button>
-//   //       <button>Crash</button>
-//   //       <button>Tom</button>
-//   //       <button>Hi-Tom</button>
-//   //       <button>Conga</button>
-//   //       <button>Hi-Conga</button>
-//   //       <button>Claves</button>
-//   //       <button>Cowbell</button>
-//   //       <button>Maracas</button>
-//   //       <button>Kick 2</button>
-//   //     </div>
-//   //     );
-//   // }
-
-// }
+let drumsArr = [
+  kick1,
+  kick2,
+  rimshot, 
+  clap, 
+  snare, 
+  cl_hihat, 
+  open_hihat, 
+  crash,
+  tom,
+  hightom,
+  conga,
+  hi_conga,
+  claves,
+  cowbell,
+  maracas
+];
